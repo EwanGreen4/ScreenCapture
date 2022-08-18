fb4253311e474737d88c33b377d4f0bc7ce83d69
Ewan Green <ewangreen95@gmail.com>
2022-08-18 13:53:49 -0600

    z

---
+++ ScreenCapture/CaptureRegionUI.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Microsoft.Graphics.Canvas.UI.Xaml;
+using Windows.UI.ViewManagement;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+using Windows.Foundation;
+
+namespace ScreenCapture {
+    internal class CaptureRegionUI {
+        private CanvasControl canvas = new CanvasControl();
+        public void promptForSelection() {
+            var thread = new Thread(() => {
+                var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+                Window.Current.SetTitleBar(null);
+                canvas.Background = new SolidColorBrush(Colors.Transparent);
+                Window.Current.Content = canvas;
+                canvas.Draw += Canvas_Draw;
+            });
+        }
+
+        private void Canvas_Draw(CanvasControl sender, CanvasDrawEventArgs args) {
+            var ds = args.DrawingSession;
+       //     SolidColorBrush c = new SolidColorBrush(Colors.Red);
+            ds.DrawRectangle(new Rect(canvas.ActualWidth / 3, canvas.ActualHeight / 3, canvas.ActualWidth / 3 * 2, canvas.ActualHeight / 3 * 2), Colors.Red);
+        }
+    }
+}
---
+++ ScreenCapture/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace ScreenCapture {
+    public static class Extensions {
+        public static IAsyncOperation<ContentDialogResult> ShowWithAnimationAsync(this ContentDialog contentDialog) { // thanks kimbra
+            if(contentDialog.Style == null) {
+                contentDialog.Style = (Style)Application.Current.Resources["DefaultContentDialogStyle"];
+            }
+            return contentDialog.ShowAsync();
+        }
+    }
+}
