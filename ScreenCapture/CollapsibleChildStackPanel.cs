68239eba0c6be089c90d2fbcbb0756a91780e065
Ewan Green <ewangreen95@gmail.com>
2022-08-26 14:35:10 -0700

    abvadafsasv

--- ScreenCapture/CollapsibleChildStackPanel.cs
+++ ScreenCapture/CollapsibleChildStackPanel.cs
@@ -7,9 +7,12 @@
 // This implementation doesn't.
 
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Media;
 
 namespace ScreenCapture {
@@ -30,18 +33,58 @@ namespace ScreenCapture {
         private void CollapsibleChildStackPanel_Loaded(object sender, object e)
             => this.SetSpacingForChildren(this.Space);
 
+        private List<Thickness> originalMargins = new();
+
+        //void UpdateMargin(DependencyObject d , DependencyPropertyChangedEventArgs a) {
+        //    FrameworkElement element = d as FrameworkElement;
+        //    var old = (Predicate<Thickness>)a.OldValue;
+
+        //    if(old != null) {
+        //        int index = originalMargins.FindIndex(old);
+        //        originalMargins.RemoveAt(index);
+        //        originalMargins.Insert(index, element.Margin);
+        //    } //else
+        //     //   originalMargins.Add(element.Margin);
+
+
+        //    SetSpacingForChildren(Space);
+        //}
+
+        //private void TrackChildMargins(FrameworkElement element) {
+        //    PropertyChangedCallback callback = UpdateMargin;
+        //    Binding bind = new Binding() { Path = new PropertyPath("Margin"), Source = element };
+        //    var property = DependencyProperty.RegisterAttached("Margin", typeof(object), typeof(CollapsibleChildStackPanel), new PropertyMetadata(null, callback));
+        //    element.SetBinding(property, bind);
+
+        //}
         private void SetSpacingForChildren(int spacing) {
-            FrameworkElement last = null;
             foreach(var it in this.Children) {
                 if(it is FrameworkElement element) {
+                    if(!originalMargins.Contains(element.Margin)) originalMargins.Add(element.Margin);
+                  //  TrackChildMargins(element);
 
-                    if(it.ActualSize.X > 0 && it.Visibility == Visibility.Visible) {
-                        
+                    var endSpacing = Space;
+                    var mainSpacing = Space / 2;
+
+                    double l = element.Margin.Left, t = element.Margin.Top, r = element.Margin.Right, b = element.Margin.Bottom;
+
+                    bool vertical = this.Orientation != Orientation.Vertical;
+                    if(it.ActualSize.X > 0 && it.ActualSize.Y > 0 && it.Visibility == Visibility.Visible) {
+                        var baseSpacing = (this.Children.IndexOf(it) > 0 && this.Children[Math.Max(this.Children.IndexOf(it) - 1, 0)].Visibility == Visibility.Visible) ? mainSpacing : 0;
+                        var footSpacing = (this.Children.IndexOf(it) < this.Children.Count - 1  && this.Children[Math.Min(this.Children.IndexOf(it) + 1, this.Children.Count - 1)].Visibility == Visibility.Visible) ? mainSpacing : 0;
+
+                        l += vertical ? baseSpacing : 0;
+                        t += vertical ? 0 : baseSpacing;
+                        r += vertical ? footSpacing : 0;
+                        b += vertical ? 0 : footSpacing;
                     } else {
-
+                        l = 0;
+                        t = 0;
+                        r = 0;
+                        b = 0;
                     }
-
-                    last = element;
+                    Trace.WriteLine($"{l} {t} {r} {b}, {element.GetType()}, {element.Name}");
+                    element.Margin = new Thickness(l, t, r, b);
                 }
             }
         }
